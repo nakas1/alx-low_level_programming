e0280347c0a963fd00e382f291a29351d12cf5ca
root <okerekechinaka@gmail.com>
2023-09-18 02:29:31 -0700

    my seventh

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: The number for which to find the square root
+ *
+ * Return: The natural square root of the number.
+ * If n does not have a natural square root, return -1.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (_find_sqrt(n, 1));
+}
+
+/**
+ * _find_sqrt - Helper function to find the square root using binary search
+ * @n: The number for which to find the square root
+ * @guess: The current guess for the square root
+ *
+ * Return: The natural square root of the number or -1 if not found.
+ */
+int _find_sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	else if (guess * guess > n)
+		return (-1);
+	else
+		return (_find_sqrt(n, guess + 1));
+
