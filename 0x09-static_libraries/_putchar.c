a078b8f6a95ca93c4fffa4230a3a2cb88b22a587
root <okerekechinaka@gmail.com>
2023-09-18 07:51:52 -0700

    my very best

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,12 @@
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ *
+ * @c: The character to be written
+ *
+ * Return: On success, the character is returned. On failure, -1 is returned,
+ * and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    /* Function logic to write 'c' to standard output goes here */
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program.
+ * It prints a quote by nakas
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
+	return (0);
+}
